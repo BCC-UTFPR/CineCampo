9f0c96c690b672d4aea37b4ff238493570296325
vinicius <vinicius@localhost.localdomain>
2016-11-29 23:35:09 -0200

    Adicionando o Navigation Drawer

--- CineCampoApp/app/src/main/java/com/example/vinicius/cinecampoapp/MainActivity.java
+++ CineCampoApp/app/src/main/java/com/example/vinicius/cinecampoapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -25,23 +26,43 @@ import com.mikepenz.materialize.util.UIUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PROFILE_SETTING = 1;
+
     private Drawer result = null;
+    private AccountHeader headerResult = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final IProfile profile = new ProfileDrawerItem().withName("Mike Penz").withIcon(R.drawable.popcorn);
+
+        headerResult = new AccountHeaderBuilder()
+                .withActivity(MainActivity.this)
+                .withTranslucentStatusBar(false)
+                .withHeaderBackground(R.color.material_drawer_dark_background)
+                .addProfiles(
+                        profile
+                )
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+
         // Handle Toolbar
         result = new DrawerBuilder()
-                .withActivity(this)
+                .withActivity(MainActivity.this)
+                .withAccountHeader(headerResult)
                 .withSavedInstance(savedInstanceState)
                 .withDisplayBelowStatusBar(false)
                 .withTranslucentStatusBar(false)
                 .withDrawerLayout(R.layout.material_drawer_fits_not)
                 .addDrawerItems(
+                        new SectionDrawerItem().withName("Menu"),
                         new PrimaryDrawerItem().withName("Filmes em exibição"),
-                        new SecondaryDrawerItem().withName("Em breve")
+                        new PrimaryDrawerItem().withName("Em breve"),
+                        new SectionDrawerItem().withName("Informações"),
+                        new PrimaryDrawerItem().withName("Contato")
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
@@ -56,5 +77,34 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(false);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //add the values which need to be saved from the drawer to the bundle
+        outState = result.saveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (result != null && result.isDrawerOpen()) {
+            result.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
