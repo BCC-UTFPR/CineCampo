2628b8fb19ea38e4065b2899807acb1a102fab03
Felipe Fronchetti <fronchettiemail@gmail.com>
2016-12-09 17:44:35 -0200

    Apagando desatualizados

--- SistemaCadastro/Tabelas/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start();
-    session_destroy();
-    unset ($_SESSION['username']);
-    unset ($_SESSION['password']);
-    header('location:index.php');
-
-?> 
--- SistemaCadastro/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start();
-    session_destroy();
-    unset ($_SESSION['usuario']);
-    unset ($_SESSION['senha']);
-    header('location:index.php');
-
-?> 
